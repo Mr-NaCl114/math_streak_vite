cd6321878028cd37fe8e1d21779861b390c73e70
Mr.NaCl <167059553+Mr-NaCl114@users.noreply.github.com>
2026-03-05 23:41:18 +0800

    fix: define __VERSION__ to prevent blank screen on startup

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  define: {
+    __VERSION__: JSON.stringify('0.0.0')
+  },
   plugins: [
     vue({
       template: {
